0fa7e85f1f0b5f689e2cd21866efa4588afb26ab
miguelrochaxavier <miguellrochaxavier@gmail.com>
2025-04-30 20:24:01 -0300

    updating api maps

--- src/api_maps.js
+++ src/api_maps.js
@@ -1,19 +1,35 @@
 const map = L.map('map').setView([-26.3045, -48.8487], 13);
 
 var layer = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}', {
-	minZoom: 0,
-	maxZoom: 20,
-	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	ext: 'png'
+    minZoom: 0,
+    maxZoom: 20,
+    attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    ext: 'png'
 });
-const positions = [[-26.3045, -48.8487], [-26.2771, -48.8627]]
-
 layer.addTo(map);
 
-const marker = L.marker(positions[0])
-marker.addTo(map)
+let clickPoints = [];
+let userMarkers = [];
+let userPolylines = [];
 
-L.marker(positions[positions.length - 1]).addTo(map)
+map.on('click', function(e) {
+    const latlng = e.latlng;
+    clickPoints.push(latlng);
 
-const polyline = L.polyline(positions, { color: 'rgb(242,211,124)'})
-polyline.addTo(map)
+    const marker = L.marker(latlng).addTo(map);
+    userMarkers.push(marker);
+
+    if (clickPoints.length === 2) {
+        const line = L.polyline(clickPoints, { color: 'rgb(242,211,124)' }).addTo(map);
+        userPolylines.push(line);
+        clickPoints = [];
+    }
+});
+
+map.on('dblclick', function() {
+    userMarkers.forEach(m => map.removeLayer(m));
+    userPolylines.forEach(l => map.removeLayer(l));
+    userMarkers = [];
+    userPolylines = [];
+    clickPoints = [];
+});
