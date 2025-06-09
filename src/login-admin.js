83f2a4b6425dc3940d5e2c5e5521a0bcabdbdce4
miguelrochaxavier <miguellrochaxavier@gmail.com>
2025-06-09 08:39:25 -0300

    updating add scripts

---
+++ src/login-admin.js
@@ -0,0 +1,10 @@
+function loginAdmin(url) {
+    username = getElementById();
+
+    if (username === "admin") {
+        alert("Bem-vindo à SmartTrain - Admin");
+    } else {
+        alert("Bem-vindo à SmartTrain");
+    }
+
+}
