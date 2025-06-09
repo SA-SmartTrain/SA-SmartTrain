eebb18d9ecfb29d6b071e72f6f83895ad200e510
miguelrochaxavier <miguellrochaxavier@gmail.com>
2025-06-09 09:11:03 -0300

    updating add starting connection php

--- bd/conexao.php
+++ bd/conexao.php
@@ -1,14 +1,13 @@
-<?php //Padrão do PHP = config.php ou conexao.php
+<?php 
 
-    $dbHost = 'localhost'; //Host padrão
-    $dbUsername = 'root'; //Username padrão
-    $dbPassword = ''; //Se tiver senha no SQL
-    $dbName = 'cadastro_miguelrocha'; //Nome do BD
-
-    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName); //conexao do BD
+    $dbHost = 'localhost'; 
+    $dbUsername = 'root'; 
+    $dbPassword = ''; 
+    $dbName = 'cadastro_miguelrocha'; 
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName); 
 
     if($conexao->connect_errno) {
         die("Conexão falhou: " . $conexao->connect_errno);
-    } //Aparecer na tela se ocorreu um erro ou não
+    } 
     
 ?>
