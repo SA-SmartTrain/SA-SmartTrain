42d325146b7a23f0d2fa520b8790cc12358d6f05
miguelrochaxavier <miguellrochaxavier@gmail.com>
2025-06-09 21:17:22 -0300

    updating add responsive @media screens

--- src/login-admin.js
+++ src/login-admin.js
@@ -1,10 +1,10 @@
-function loginAdmin(url) {
-    username = getElementById();
+function loginAdmin() {
+    const username = document.getElementById('username').value.trim().toLowerCase();
 
-    if (username === "admin") {
-        alert("Bem-vindo à SmartTrain - Admin");
+    if (username === 'admin') {
+        alert('Bem-vindo à SmartTrain | Admin');
+        window.location.href = '.../public/pagina_inicial.html';
     } else {
-        alert("Bem-vindo à SmartTrain");
+        alert('Bem-vindo à SmartTrain');
     }
-
 }
