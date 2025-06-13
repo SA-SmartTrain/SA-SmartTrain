5d42f67ada47c146cf214506d7f5edf8d186db90
miguelrochaxavier <miguellrochaxavier@gmail.com>
2025-06-13 17:30:07 -0300

    updating files

--- src/assets/tela_carregamento_waiting.js
+++ src/assets/tela_carregamento_waiting.js
@@ -1,7 +1,7 @@
 function Loading_url_tela_incial_for_pagina_incial(url) {
     setTimeout(function () {
         window.location.href = url;
-    }, 5000); 
+    }, 2000); 
 }
 
 Loading_url_tela_incial_for_pagina_incial("../public/login/cadastro-se-dados.html");
