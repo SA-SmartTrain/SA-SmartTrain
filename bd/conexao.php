f2a317ca5fd092d886544d6a0ef2b26cf1808cac
miguelrochaxavier <miguellrochaxavier@gmail.com>
2025-06-13 18:31:07 -0300

    updating connection php

--- bd/conexao.php
+++ bd/conexao.php
@@ -2,13 +2,17 @@
 
 /*Adicionar o ini php em casa no db*/
 
+$config = parse_ini_file('config.ini', true);
+
 $inipath = php_ini_loaded_file();
-$ini_array = parse_ini_file($inipath, true)["PHP"];
+$ini_array = parse_ini_file('config.ini', true);
+
+$dbConfig = $config['database'];
 
 $dbName = 'smarttrain';
 $dbUsername = 'smarttrain';
-$dbPassword = $ini_array[""];
-$dbHost = $ini_array[""];
+$dbPassword = $ini_array["database"]["password"];
+$dbHost = $ini_array["database"]["host"];
 $dbPort = 6306;
 
 $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName, $dbPort);
