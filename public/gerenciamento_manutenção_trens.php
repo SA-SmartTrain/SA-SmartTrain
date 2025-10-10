0822a384dd0aaa552a1c2c0c5e0e9d513753a763
iasmin-rabelo20 <iasmin_rabelo@estudante.sesisenai.org.br>
2025-10-10 11:52:56 -0300

    mudança do nome

--- public/gerenciamento_manutenção_trens.php
+++ public/gerenciamento_manutenção_trens.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="../src/assets/logo/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="../style/gerenciamento_manutenção_trens.css">
-    <title>SmartTrain - Gerenciamento de Sensores</title>
+    <title>SmartTrain - Gerenciamento de Manutenções dos Trens</title>
 </head>
 
 <body>
